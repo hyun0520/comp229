8d7bf720540ce8544f80247a02132fb3863efe2f
hyun0520 <chunghyun0520@naver.com>
2025-05-15 16:05:09 +0900

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <ul>
+                <li>Teaching comp229</li>
+                <li>Developing new Software</li>
+            </ul>
+        </div>
+
+    )
+}
